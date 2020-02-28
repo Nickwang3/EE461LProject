934d29ba0c632e600b2488b0f73f6a9d4aabf902
nick wang <nickwang508@gmail.com>
2020-02-28 11:31:56 -0600

    updating config settings for local and prod hosting

--- apiproject/apiproject/urls.py
+++ apiproject/apiproject/urls.py
@@ -5,15 +5,15 @@ from django.contrib import admin
 
 #register api endpoints and their views
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'authors', views.AuthorViewSet)
-router.register(r'books', views.BookViewSet)
-router.register(r'reviews', views.ReviewViewSet)
-router.register(r'meetups', views.MeetupViewSet)
+router.register(r'users/getusers', views.UserViewSet)
+router.register(r'authors/getauthors', views.AuthorViewSet)
+router.register(r'books/getbooks', views.BookViewSet)
+router.register(r'reviews/getreviews', views.ReviewViewSet)
+router.register(r'meetups/getmeetups', views.MeetupViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
