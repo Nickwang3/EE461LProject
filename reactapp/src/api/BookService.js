f83317743e57b1322ee864a7699928301699f7c1
nick wang <nickwang508@gmail.com>
2020-03-01 15:10:58 -0600

    change allowed hosts in api to allow requests from s3 host endpoint

--- reactapp/src/api/BookService.js
+++ reactapp/src/api/BookService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_URL = "http://ec2-13-59-7-216.us-east-2.compute.amazonaws.com/api/v1";
-axios.defaults.baseURL = "http://localhost:8000/api/v1"
+// axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "http://ec2-13-59-7-216.us-east-2.compute.amazonaws.com/api/v1";
 
 export default class BookService {
   constructor() {}
