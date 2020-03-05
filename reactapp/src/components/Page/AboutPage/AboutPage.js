4d85bb57406848839f5ae00ed7ae71b130e07a3b
aannestrand <aannestrand98@gmail.com>
2020-03-05 11:59:16 -0600

    Added first conrtibutor card to about page, need to render all still

--- reactapp/src/components/Page/AboutPage/AboutPage.js
+++ reactapp/src/components/Page/AboutPage/AboutPage.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
 
 class AboutPage extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
             error: null,
-            isLoaded: false
+            isLoaded: false,
+            name: null,
+            avatar: null,
+            contributions: null,
+            // issues: null
         }
+
+        this.setData = this.setData.bind(this);
     }
+  
+  
+    setData(name, contributions, avatar) {
+        this.setState({avatar: avatar});
+        this.setState({name: name});
+        this.setState({contributions: contributions});
+        console.log(avatar)
+    };
+
+
 
     componentDidMount() {
+
+        fetch('https://api.github.com/repos/Nickwang3/EE461LProject/contributors')
+            .then(res => res.json())
+            .then(data => {this.setData(data[0].login, data[0].contributions, data[0].avatar_url)});
+
         this.setState({
-            isLoaded: true
+            isLoaded: true,
         })
     }
 
@@ -24,7 +51,14 @@ class AboutPage extends React.Component {
         } else {
             return (
                 <div>
-                    <h1>About our Project</h1>
+                    <Card>
+                        <CardImg top width="20%" src={this.state.avatar} alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle style={{color:"black"}}>{this.state.name}</CardTitle>
+                            {/* <CardSubtitle>Card subtitle</CardSubtitle> */}
+                            <CardText style={{color:"black"}}>Contributions: {this.state.contributions}</CardText>
+                        </CardBody>
+                    </Card>
                 </div>
             )
         }
