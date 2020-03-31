fb22cda1b9d715a082ebf741b57b09a94e525195
JacobMedeiros <jacobmedeiros16@gmail.com>
2020-03-30 22:32:03 -0500

    Added django testing

--- reactapp/src/api/ApiService.js
+++ reactapp/src/api/ApiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// axios.defaults.baseURL = "http://localhost:8000/api/v1";
-axios.defaults.baseURL = "http://django-env.zphgcpmf2t.us-west-2.elasticbeanstalk.com/api/v1";
+ axios.defaults.baseURL = "http://localhost:8000/api/v1";
+//axios.defaults.baseURL = "http://django-env.zphgcpmf2t.us-west-2.elasticbeanstalk.com/api/v1";
 
 export default class ApiService {
   constructor() {}
