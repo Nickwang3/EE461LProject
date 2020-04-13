3f08cf083ad2986c9e0b60d5a0d62bc095731b8a
aannestrand <aannestrand98@gmail.com>
2020-04-13 17:06:09 -0500

    Fixed minor error with linking on detailedteamspage

--- reactapp/src/components/Page/ScoresPage/Scoreboard.js
+++ reactapp/src/components/Page/ScoresPage/Scoreboard.js
@@ -62,7 +62,7 @@ class ScoreBoard extends React.Component {
                     <Row className="homeTeamRow">
                         
                         <Col className="teamPictureCol">
-                            <Link to={`teams/${homeTeam.team_id}`}><img style={{height: "100px", width: "90px"}} src={homeTeam.logo}></img></Link>
+                            <a href={`/teams/${homeTeam.team_id}`}><img style={{height: "100px", width: "90px"}} src={homeTeam.logo}></img></a>
                         </Col>
 
                         <Col className="teamNameCol">
@@ -82,7 +82,7 @@ class ScoreBoard extends React.Component {
                     <Row className="awayTeamRow">
 
                         <Col className="teamPictureCol">
-                            <Link to={`teams/${awayTeam.team_id}`}><img style={{height: "100px", width: "90px"}} src={awayTeam.logo}></img></Link>
+                            <a href={`/teams/${awayTeam.team_id}`}><img style={{height: "100px", width: "90px"}} src={awayTeam.logo}></img></a>
                         </Col>
 
                         <Col className="teamNameCol">
