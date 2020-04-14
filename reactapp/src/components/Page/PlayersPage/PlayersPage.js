16ca3134253c7b3b9a0f28d83184e0dcda9d14ae
Nick Wang <nickwang508@gmail.com>
2020-04-14 16:04:27 -0500

    doesnt refresh entire page when searching and sorting players fixed other bugs related to paging and searching with the component's state

--- reactapp/src/components/Page/PlayersPage/PlayersPage.js
+++ reactapp/src/components/Page/PlayersPage/PlayersPage.js
@@ -27,7 +27,7 @@ class PlayersPage extends React.Component {
   componentDidMount() {
     this._isMounted = true;
     apiService
-      .getPlayers(this.state.page, this.state.ordering)
+      .getPlayersBySearch(this.state.page, this.state.searchValue, this.state.searchFields, this.state.ordering)
       .then(result => {
         this.setState({
           isLoaded: true,
@@ -53,7 +53,7 @@ class PlayersPage extends React.Component {
       isLoaded: false,
     })
     apiService
-      .getPlayers(this.state.page + 1)
+      .getPlayersBySearch(this.state.page + 1, this.state.searchValue, this.state.searchFields, this.state.ordering)
       .then(result => {
         this.setState({
           isLoaded: true,
@@ -79,7 +79,7 @@ class PlayersPage extends React.Component {
       isLoaded: false,
     })
     apiService
-      .getPlayers(this.state.page - 1)
+      .getPlayersBySearch(this.state.page - 1, this.state.searchValue, this.state.searchFields, this.state.ordering)
       .then(result => {
         this.setState({
           isLoaded: true,
@@ -103,11 +103,12 @@ class PlayersPage extends React.Component {
       isLoaded: false,
     })
     apiService
-    .getPlayersBySearch(this.state.page, this.state.searchValue, this.state.searchFields, this.state.ordering)
+    .getPlayersBySearch(1, this.state.searchValue, this.state.searchFields, this.state.ordering)
     .then(result => {
       this.setState({
         isLoaded: true,
         players: result.data.results,
+        page: 1,
         prevPage: result.data.previous,
         nextPage: result.data.next,
         count: result.data.count
@@ -130,73 +131,77 @@ class PlayersPage extends React.Component {
 
   render() {
     const { error, isLoaded, players } = this.state;
+    let results;
     if (error) {
-      return <div>Error: {error.message}</div>;
+      results = <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      results = <div>Loading...</div>;
     } else {
-      return (
-        <div className="PlayersPage">
-          <h1>MLB Players</h1>
-          <Form className="searchBarContainer" onSubmit={this.onSubmit}>
-            <FormGroup className="searchBar">
-                {/* <Label for="exampleSearch">Search</Label> */}
-                <Input
-                type="search"
-                name="search"
-                id="playerSearch"
-                placeholder="Search for players..."
-                value={this.state.searchValue}
-                onChange={e => this.setState({ searchValue: e.target.value })}
-                onKeyDown={this.onEnterPressed}
-                />
-            </FormGroup>
-            <FormGroup>
-              <Label for="playerSearchSelect">search by</Label>
-              <Input 
-              type="select" 
-              name="searchSelect" 
-              id="playerSearchSelect"
-              onChange={e => this.setState({ searchFields: e.target.value })}
-              >
-                <option>name</option>
-                <option>age</option>
-                <option>number</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="playerOrderSelect">order by</Label>
-              <Input 
-              type="select" 
-              name="playerOrderSelect" 
-              id="playerOrderSelect"
-              onChange={e => this.setState({ ordering: e.target.value })}
-              >
-                <option>name</option>
-                <option>team</option>
-                <option>age</option>
-                <option>number</option>
-                <option>height</option>
-                <option>weight</option>
-                <option>player_id</option>
-              </Input>
-            </FormGroup>
-            <Button type="submit" className="btn btn-success">Search</Button>
-          </Form>               
-          <h5>Results: {this.state.count}</h5>
-          <Container>
-            {players.map(player => (
-              <Player player={player}/>
-            ))}
-          </Container>
-          <Container>
-            <Button color="info" onClick={() => this.prevPage()} disabled={this.state.prevPage == null}>Previous</Button>
-            <h5>Current Page: {this.state.page}</h5>
-            <Button color="info" onClick={() => this.nextPage()} disabled={this.state.nextPage == null}>Next</Button>
-          </Container>
-        </div>
-      );
+      results = <div>
+                  <h5>Results: {this.state.count}</h5>
+                    <Container>
+                      {players.map(player => (
+                        <Player player={player}/>
+                      ))}
+                    </Container>
+                </div>;
     }
+    return (
+      <div className="PlayersPage">
+        <h1>MLB Players</h1>
+        <Form className="searchBarContainer" onSubmit={this.onSubmit}>
+          <FormGroup className="searchBar">
+              {/* <Label for="exampleSearch">Search</Label> */}
+              <Input
+              type="search"
+              name="search"
+              id="playerSearch"
+              placeholder="Search for players..."
+              value={this.state.searchValue}
+              onChange={e => this.setState({ searchValue: e.target.value })}
+              onKeyDown={this.onEnterPressed}
+              />
+          </FormGroup>
+          <FormGroup>
+            <Label for="playerSearchSelect">search by</Label>
+            <Input 
+            type="select" 
+            name="searchSelect" 
+            id="playerSearchSelect"
+            onChange={e => this.setState({ searchFields: e.target.value })}
+            >
+              <option>name</option>
+              <option>age</option>
+              <option>number</option>
+            </Input>
+          </FormGroup>
+          <FormGroup>
+            <Label for="playerOrderSelect">order by</Label>
+            <Input 
+            type="select" 
+            name="playerOrderSelect" 
+            id="playerOrderSelect"
+            onChange={e => this.setState({ ordering: e.target.value })}
+            >
+              <option>name</option>
+              <option>team</option>
+              <option>age</option>
+              <option>number</option>
+              <option>height</option>
+              <option>weight</option>
+              <option>player_id</option>
+            </Input>
+          </FormGroup>
+          <Button type="submit" className="btn btn-success">Search</Button>
+        </Form>               
+        {results}
+        <Container>
+          <Button color="info" onClick={() => this.prevPage()} disabled={this.state.prevPage == null}>Previous</Button>
+          <h5>Current Page: {this.state.page}</h5>
+          <Button color="info" onClick={() => this.nextPage()} disabled={this.state.nextPage == null}>Next</Button>
+        </Container>
+      </div>
+      );
   }
 
 }
