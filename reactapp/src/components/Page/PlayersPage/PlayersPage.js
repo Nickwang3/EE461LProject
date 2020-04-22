44a79c79d7d6e815150531333ed38d103ad2a910
aannestrand <aannestrand98@gmail.com>
2020-04-21 23:33:21 -0500

    Boxscore's frontend updated, pagination and search bars neater

--- reactapp/src/components/Page/PlayersPage/PlayersPage.js
+++ reactapp/src/components/Page/PlayersPage/PlayersPage.js
@@ -174,58 +174,71 @@ class PlayersPage extends React.Component {
     }
     return (
       <Container className="playersPageContainer">
-        <Row style={{margin:"40px", width:"100%", display:"flex", justifyContent:"center"}}><h1 style={{font: "400 50px/1.5 'Pacifico', Helvetica, sans-serif"}}>Players</h1></Row>
-          <Form className="searchBarContainer" onSubmit={this.onSubmit}>
-            <FormGroup className="searchBar">
-                {/* <Label for="exampleSearch">Search</Label> */}
-                <Input
-                type="search"
-                name="search"
-                id="playerSearch"
-                placeholder="Search for players..."
-                value={this.state.searchValue}
-                onChange={e => this.setState({ searchValue: e.target.value })}
-                onKeyDown={this.onEnterPressed}
-                />
-            </FormGroup>
-            <FormGroup>
-              <Label for="playerSearchSelect">search by</Label>
-              <Input 
-              type="select" 
-              name="searchSelect" 
-              id="playerSearchSelect"
-              onChange={e => this.setState({ searchFields: e.target.value })}
-              >
-                <option>name</option>
-                <option>age</option>
-                <option>number</option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-              <Label for="playerOrderSelect">order by</Label>
-              <Input 
-              type="select" 
-              name="playerOrderSelect" 
-              id="playerOrderSelect"
-              onChange={e => this.orderingChanged(e)}
-              >
-                <option>name</option>
-                <option>team</option>
-                <option>age</option>
-                <option>number</option>
-                <option>height</option>
-                <option>weight</option>
-                <option>player_id</option>
-              </Input>
-            </FormGroup>
-            <Button type="submit" className="btn btn-success">Search</Button>
+        <Row style={{margin:"60px", width:"100%", display:"flex", justifyContent:"center"}}><h1 style={{font: "400 50px/1.5 'Pacifico', Helvetica, sans-serif"}}>Players</h1></Row>
+          <Form className="playerFormStyle" onSubmit={this.onSubmit}>
+            <Row form>
+              <Col md={9}>
+                <FormGroup className="playerSearchBar">
+                    {/* <Label for="exampleSearch">Search</Label> */}
+                    <Input
+                    type="search"
+                    name="search"
+                    id="playerSearch"
+                    placeholder="Search for players..."
+                    value={this.state.searchValue}
+                    onChange={e => this.setState({ searchValue: e.target.value })}
+                    onKeyDown={this.onEnterPressed}
+                    style={{width: "100%"}}
+                    />
+                </FormGroup>
+              </Col>
+              <Col style={{display:"flex", justifyContent: "center"}} md={3}>
+                <Button className="playerSearchButtonStyle" style={{width: "80%", height: "70%"}} type="submit" className="btn btn-success">Search</Button>
+              </Col>
+            </Row>
+            <Row form>
+              <Col md={6}>
+                <FormGroup >
+                  <Label style={{fontSize: "medium"}} for="playerSearchSelect">Search by</Label>
+                  <Input 
+                  type="select" 
+                  name="searchSelect" 
+                  id="playerSearchSelect"
+                  onChange={e => this.setState({ searchFields: e.target.value })}
+                  >
+                    <option>name</option>
+                    <option>age</option>
+                    <option>number</option>
+                  </Input>
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label style={{fontSize: "medium"}} for="playerOrderSelect">Order by</Label>
+                  <Input 
+                  type="select" 
+                  name="playerOrderSelect" 
+                  id="playerOrderSelect"
+                  onChange={e => this.orderingChanged(e)}
+                  >
+                    <option>name</option>
+                    <option>team</option>
+                    <option>age</option>
+                    <option>number</option>
+                    <option>height</option>
+                    <option>weight</option>
+                    <option>player_id</option>
+                  </Input>
+                </FormGroup>
+              </Col>
+            </Row>
           </Form>   
         {results}
-        <Container>
-          <Button color="info" onClick={() => this.prevPage()} disabled={this.state.prevPage == null}>Previous</Button>
-          <h5>Current Page: {this.state.page}</h5>
-          <Button color="info" onClick={() => this.nextPage()} disabled={this.state.nextPage == null}>Next</Button>
-        </Container>
+        <Row style={{width: "100%", display: "flex", justifyContent: "center"}}>
+          <Button style={{margin: "20px"}} color="info" onClick={() => this.prevPage()} disabled={this.state.prevPage == null}>Previous</Button>
+          <h4 style={{margin: "23px"}}>Page {this.state.page}</h4>
+          <Button style={{margin: "20px"}} color="info" onClick={() => this.nextPage()} disabled={this.state.nextPage == null}>Next</Button>
+        </Row>
       </Container>
       );
   }
