522b8dc7097fdb7ad0bf9bb29bf2f955e75f4ea0
Nick Wang <nickwang508@gmail.com>
2020-04-30 00:14:44 -0500

    basic implementation of controller component for searching

--- reactapp/src/components/Page/PlayersPage/PlayersPage.js
+++ reactapp/src/components/Page/PlayersPage/PlayersPage.js
@@ -2,9 +2,8 @@ import React from "react";
 import Player from "./Player";
 import ApiService from "../../../api/ApiService";
 import { Container, Button, Row , Form, FormGroup, Input, Label, Col, Spinner} from "reactstrap"
-import ReactPaginate from 'react-paginate';
-import Axios from "axios";
 import "./PlayersPage.css"
+import SearchController from "../../Search/SearchController";
 
 const apiService = ApiService.getInstance();
 
@@ -98,61 +97,70 @@ class PlayersPage extends React.Component {
       });
   }
 
-  onSubmit = (e) => {
-    e.preventDefault()
+  // onSubmit = (e) => {
+  //   e.preventDefault()
+  //   this.setState({
+  //     isLoaded: false,
+  //   })
+  //   apiService
+  //   .getPlayersBySearch(1, this.state.searchValue, this.state.searchFields, this.state.ordering)
+  //   .then(result => {
+  //     this.setState({
+  //       isLoaded: true,
+  //       players: result.data.results,
+  //       page: 1,
+  //       prevPage: result.data.previous,
+  //       nextPage: result.data.next,
+  //       count: result.data.count
+  //     });
+  //   })
+  //   .catch(error => {
+  //     this.setState({
+  //       isLoaded: true,
+  //       error
+  //     });
+  //   });
+  // }
+
+  // onEnterPressed = (e) => {
+  //   if (e.key === 'Enter') {
+  //     e.preventDefault(); 
+  //     this.onSubmit(e);
+  //   }
+  // }
+
+  // orderingChanged = (e) => {
+  //   this.setState({ 
+  //     ordering: e.target.value,
+  //     isLoaded: false
+  //   })
+  //   apiService
+  //   .getPlayersBySearch(1, this.state.searchValue, this.state.searchFields, e.target.value)
+  //   .then(result => {
+  //     this.setState({
+  //       isLoaded: true,
+  //       players: result.data.results,
+  //       page: 1,
+  //       prevPage: result.data.previous,
+  //       nextPage: result.data.next,
+  //       count: result.data.count
+  //     });
+  //   })
+  //   .catch(error => {
+  //     this.setState({
+  //       isLoaded: true,
+  //       error
+  //     });
+  //   });
+  // }
+
+  setResults(results, isLoaded, error) {
     this.setState({
-      isLoaded: false,
-    })
-    apiService
-    .getPlayersBySearch(1, this.state.searchValue, this.state.searchFields, this.state.ordering)
-    .then(result => {
-      this.setState({
-        isLoaded: true,
-        players: result.data.results,
-        page: 1,
-        prevPage: result.data.previous,
-        nextPage: result.data.next,
-        count: result.data.count
-      });
-    })
-    .catch(error => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    });
-  }
+      isLoaded: isLoaded,
+      error: error,
+      players: results,
 
-  onEnterPressed = (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault(); 
-      this.onSubmit(e);
-    }
-  }
-
-  orderingChanged = (e) => {
-    this.setState({ 
-      ordering: e.target.value,
-      isLoaded: false
     })
-    apiService
-    .getPlayersBySearch(1, this.state.searchValue, this.state.searchFields, e.target.value)
-    .then(result => {
-      this.setState({
-        isLoaded: true,
-        players: result.data.results,
-        page: 1,
-        prevPage: result.data.previous,
-        nextPage: result.data.next,
-        count: result.data.count
-      });
-    })
-    .catch(error => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    });
   }
 
   render() {
@@ -179,64 +187,12 @@ class PlayersPage extends React.Component {
     return (
       <Container className="playersPageContainer">
         <Row style={{margin:"60px", width:"100%", display:"flex", justifyContent:"center"}}><h1 style={{font: "400 50px/1.5 'Pacifico', Helvetica, sans-serif"}}>Players</h1></Row>
-          <Form className="playerFormStyle" onSubmit={this.onSubmit}>
-            <Row form>
-              <Col md={9}>
-                <FormGroup className="playerSearchBar">
-                    {/* <Label for="exampleSearch">Search</Label> */}
-                    <Input
-                    type="search"
-                    name="search"
-                    id="playerSearch"
-                    placeholder="Search for players..."
-                    value={this.state.searchValue}
-                    onChange={e => this.setState({ searchValue: e.target.value })}
-                    onKeyDown={this.onEnterPressed}
-                    style={{width: "100%"}}
-                    />
-                </FormGroup>
-              </Col>
-              <Col style={{display:"flex", justifyContent: "center"}} md={3}>
-                <Button className="playerSearchButtonStyle" style={{width: "80%", height: "70%"}} type="submit" className="btn btn-success">Search</Button>
-              </Col>
-            </Row>
-            <Row form>
-              <Col md={6}>
-                <FormGroup >
-                  <Label style={{fontSize: "medium"}} for="playerSearchSelect">Search by</Label>
-                  <Input 
-                  type="select" 
-                  name="searchSelect" 
-                  id="playerSearchSelect"
-                  onChange={e => this.setState({ searchFields: e.target.value })}
-                  >
-                    <option>name</option>
-                    <option>age</option>
-                    <option>number</option>
-                  </Input>
-                </FormGroup>
-              </Col>
-              <Col md={6}>
-                <FormGroup>
-                  <Label style={{fontSize: "medium"}} for="playerOrderSelect">Order by</Label>
-                  <Input 
-                  type="select" 
-                  name="playerOrderSelect" 
-                  id="playerOrderSelect"
-                  onChange={e => this.orderingChanged(e)}
-                  >
-                    <option>name</option>
-                    <option>team</option>
-                    <option>age</option>
-                    <option>number</option>
-                    <option>height</option>
-                    <option>weight</option>
-                    <option>player_id</option>
-                  </Input>
-                </FormGroup>
-              </Col>
-            </Row>
-          </Form>   
+        <SearchController
+          getResults={apiService.getPlayersBySearch.bind(apiService)}
+          defaultSearchField="name"
+          defaultOrdering="name"
+          setResults={this.setResults.bind(this)}
+        />
         {results}
         <Row style={{width: "100%", display: "flex", justifyContent: "center"}}>
           <Button style={{margin: "20px"}} color="info" onClick={() => this.prevPage()} disabled={this.state.prevPage == null}>Previous</Button>
---
+++ reactapp/src/components/Search/SearchController.js
@@ -0,0 +1,131 @@
+import React from "react";
+import { Button, Row , Form, FormGroup, Input, Label, Col} from "reactstrap"
+
+class SearchController extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          results: [],
+          page: 1,
+          prevPage: null,
+          nextPage: null,
+          count: null,
+          searchValue: '', 
+          searchFields: this.props.defaultSearchField,
+          ordering: this.props.defaultOrdering,
+        };
+    }
+
+
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.setResults([], false, null);
+        this.props.getResults(1, this.state.searchValue, this.state.searchFields, this.state.ordering)
+        .then(result => {
+          this.setState({
+            isLoaded: true,
+            results: result.data.results,
+            page: 1,
+            prevPage: result.data.previous,
+            nextPage: result.data.next,
+            count: result.data.count
+          });
+          this.props.setResults(result.data.results, true, null);
+        })
+        .catch(error => {
+            this.props.setResults(null, true, error);
+        });
+    }
+    
+    onEnterPressed = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault(); 
+            this.onSubmit(e);
+        }
+    }
+    
+    orderingChanged = (e) => {
+        this.setState({ 
+            ordering: e.target.value,
+        })
+        this.props.setResults([], false, null);
+        this.props.getResults(1, this.state.searchValue, this.state.searchFields, e.target.value)
+        .then(result => {
+            this.setState({
+            isLoaded: true,
+            results: result.data.results,
+            page: 1,
+            prevPage: result.data.previous,
+            nextPage: result.data.next,
+            count: result.data.count
+            });
+            this.props.setResults(result.data.results, true, null);
+        })
+        .catch(error => {
+            this.props.setResults(null, true, error);
+        });
+    }
+
+    render() {
+        return (
+                <Form className="playerFormStyle" onSubmit={this.onSubmit}>
+                    <Row form>
+                    <Col md={9}>
+                        <FormGroup className="playerSearchBar">
+                            <Input
+                            type="search"
+                            name="search"
+                            id="playerSearch"
+                            placeholder="Search for players..."
+                            value={this.state.searchValue}
+                            onChange={e => this.setState({ searchValue: e.target.value })}
+                            onKeyDown={this.onEnterPressed}
+                            style={{width: "100%"}}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col style={{display:"flex", justifyContent: "center"}} md={3}>
+                        <Button className="playerSearchButtonStyle" style={{width: "80%", height: "70%"}} type="submit" className="btn btn-success">Search</Button>
+                    </Col>
+                    </Row>
+                    <Row form>
+                    <Col md={6}>
+                        <FormGroup >
+                        <Label style={{fontSize: "medium"}} for="playerSearchSelect">Search by</Label>
+                        <Input 
+                        type="select" 
+                        name="searchSelect" 
+                        id="playerSearchSelect"
+                        onChange={e => this.setState({ searchFields: e.target.value })}
+                        >
+                            <option>name</option>
+                            <option>age</option>
+                            <option>number</option>
+                        </Input>
+                        </FormGroup>
+                    </Col>
+                    <Col md={6}>
+                        <FormGroup>
+                        <Label style={{fontSize: "medium"}} for="playerOrderSelect">Order by</Label>
+                        <Input 
+                        type="select" 
+                        name="playerOrderSelect" 
+                        id="playerOrderSelect"
+                        onChange={e => this.orderingChanged(e)}
+                        >
+                            <option>name</option>
+                            <option>team</option>
+                            <option>age</option>
+                            <option>number</option>
+                            <option>height</option>
+                            <option>weight</option>
+                            <option>player_id</option>
+                        </Input>
+                        </FormGroup>
+                    </Col>
+                    </Row>
+                </Form>   
+            );
+    }
+} export default SearchController;
