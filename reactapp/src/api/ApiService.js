9f72414de1290936eac96415314d8a8596ce1fb6
Joseph John Tombari <josephTombari@gmail.com>
2020-05-01 15:00:13 -0500

    fix API service

--- reactapp/src/api/ApiService.js
+++ reactapp/src/api/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api/v1";
-//axios.defaults.baseURL = "http://django-env.zphgcpmf2t.us-west-2.elasticbeanstalk.com/api/v1";
+//axios.defaults.baseURL = "http://localhost:8000/api/v1";
+axios.defaults.baseURL = "http://django-env.zphgcpmf2t.us-west-2.elasticbeanstalk.com/api/v1";
 
 export default class ApiService {
 
